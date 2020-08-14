4a66ec147732bd159b3e16a245bcfc911f8cbf57
Esdra <esdrasantos11@gmail.com>
2020-08-13 23:27:56 -0300

    Ex1Aula3
    Futricando duas classes

---
+++ Aula 3/Exercise1/Resolução Esdra/CriaTriangulos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CriadorDeFormas;
+using Geometria;
+namespace Resolução_Esdra
+{
+    class CriaTriangulos
+    {
+        static void Main(string[] args)
+        {
+            // Nao eh necessaria instanciar um objeto da classe formador 
+            // pois o metodo formarTriangulo eh estatico e sendo assim, 
+            // eh atrelado a classe e nao ao objeto
+            forma poligono = formador.formarTriangulo(15);
+            poligono.perimetro();
+
+        }
+    }
+}
