929c221e0c118c9dff3938b979b34e35aa2968eb
Esdra <esdrasantos11@gmail.com>
2020-08-14 00:53:00 -0300

    Ex2Aula3_Resolucao
    Minha resolução e documentação do exercicio 2.

--- Aula 3/Exercise2/Resolução Esdra/formador.cs
+++ Aula 3/Exercise2/Resolução Esdra/formador.cs
@@ -4,19 +4,12 @@ namespace CriadorDeFormas
 {
     class formador
     {
-        ///<summary>
-        /// Metodo estatico que retorna um triangulo 
-        ///</summary>
         public static forma formarTriangulo(int TamanhoLados)
-        {   
-            // Declara um objeto do tipo forma
+        {
             forma triangulo;
-            // Instancia o objeto
             triangulo = new forma();
-            // Altera atributos privados do objeto atraves de metodos publicos
             triangulo.setLados(3);
             triangulo.setTamanho(TamanhoLados);
-            // Retorna o objeto com os atributos setados
             return triangulo;
         } 
     }
